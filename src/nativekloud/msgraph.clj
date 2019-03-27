6301aa343b5365bac062e43214b644dd5ee8c3c9
Radek Dymacz <radek@Radeks-MacBook-Pro-1218.local>
2019-03-27 17:01:51 +0100

    pass token around

--- src/nativekloud/msgraph.clj
+++ src/nativekloud/msgraph.clj
@@ -6,31 +6,6 @@
             [slingshot.slingshot :refer [throw+ try+]]))
 
 
-
-(def settings (atom  {:params     { :client_id    ""
-                                   :client_secret ""
-                                   :scope         ["https://graph.microsoft.com/.default"]
-                                   :grant_type    "client_credentials"
-                                   }
-                      :tenant_id ""
-                      :token      nil}))
-
-
-;;; Settings
-
-(defn set-client_id! [client_id]
-  (swap! settings assoc-in [:params :client_id] client_id))
-
-(defn set-client_secret! [client_secret]
-  (swap! settings assoc-in [:params :client_secret] client_secret))
-
-(defn set-tenant_id! [tenant_id]
-  (swap! settings assoc-in [:tenant_id] tenant_id))
-
-
-(defn dump-settings []
-  (print "Settings atom: " @settings))
-
 ;;; API
 
 
@@ -52,30 +27,20 @@
   (str "https://login.microsoftonline.com/" tenant_id "/oauth2/v2.0/token"))
 
 
-(defn get-token []
-  (:body (http/post (api-token-url (:tenant_id @settings))
-                    {:form-params  (:params @settings)
+(defn get-token [config]
+  (:body (http/post (api-token-url (:tenant_id config))
+                    {:form-params  (:params config)
                      :as                 :json
                      :timeout            5
                      :connection-manager cm
                      :throw-exceptions   true})))
 
-(defn set-token! []
-  (swap! settings assoc-in [:token]  (get-token)))
-
-(defn set-params! [config]
-  (set-client_id! (get-in config [:params :client_id]))
-  (set-client_secret! (get-in config [:params :client_secret]))
-  (set-tenant_id! (:tenant_id config))
-  (set-token!)
-  )
-
 ;; API clj-http
-(defn call-api [url]
+(defn call-api [url token]
   (safely
     (try+
    ;(log/info "Requesting: " (trunc url 200) "...")
-   (http/get url {:oauth-token        (:access_token (:token @settings))
+   (http/get url {:oauth-token        (:access_token token)
                   :as                 :json
                   :debug              false
                   :throw-exceptions   true
@@ -101,129 +66,131 @@
 (defn api-get
   "Calls msgraph API and handles paged results.
   Returns vector of results"
-  [url]
+  [url token]
   (let [url (str api-base-url url)]
-    (loop [response (:body (call-api url))
+    (loop [response (:body (call-api url token))
            results  []]
       (if (nil? ((keyword "@odata.nextLink") response))
         (concat results (:value response))
-        (recur (:body (call-api ((keyword "@odata.nextLink") response)))
+        (recur (:body (call-api ((keyword "@odata.nextLink") response) token))
                (concat results (:value response)))))))
 
-(defn api-get-callback
-  "Calls msgraph API and handles paged results.
-  Returns vector of results"
-  [url user fn]
-  (let [url (str api-base-url url)]
-    (loop [response (:body (call-api url))
-           results  (fn user (:value response))]
-      (if (nil? ((keyword "@odata.nextLink") response))
-        (fn user (:value response))
-        (recur (:body (call-api ((keyword "@odata.nextLink") response)))
-               (fn user (:value response)))))))
+;; (defn api-get-callback
+;;   "Calls msgraph API and handles paged results.
+;;   Returns vector of results"
+;;   [url user fn]
+;;   (let [url (str api-base-url url)]
+;;     (loop [response (:body (call-api url))
+;;            results  (fn user (:value response))]
+;;       (if (nil? ((keyword "@odata.nextLink") response))
+;;         (fn user (:value response))
+;;         (recur (:body (call-api ((keyword "@odata.nextLink") response)))
+;;                (fn user (:value response)))))))
 
 
-(defn api-get-delta
-  "Delta query enables applications to discover newly created, updated, or deleted
-  entities without performing a full read of the target resource with every request.
-  Microsoft Graph applications can use delta query to efficiently synchronize changes
-  with a local data store.
-  See docs at https://docs.microsoft.com/en-us/graph/delta-query-overview
-  Returns map {:resuls [] :deltaLink url} " 
-  [url]
-  (loop [response (:body (call-api url))
-         results  []]
-    (if (nil? ((keyword "@odata.nextLink") response))
-      {:results  (concat results (:value response))
-       :deltaLink ((keyword "@odata.deltaLink") response)} 
-      (recur (:body (call-api ((keyword "@odata.nextLink") response)))
-             (concat results (:value response))))))
+;; (defn api-get-delta
+;;   "Delta query enables applications to discover newly created, updated, or deleted
+;;   entities without performing a full read of the target resource with every request.
+;;   Microsoft Graph applications can use delta query to efficiently synchronize changes
+;;   with a local data store.
+;;   See docs at https://docs.microsoft.com/en-us/graph/delta-query-overview
+;;   Returns map {:resuls [] :deltaLink url} " 
+;;   [url]
+;;   (loop [response (:body (call-api url))
+;;          results  []]
+;;     (if (nil? ((keyword "@odata.nextLink") response))
+;;       {:results  (concat results (:value response))
+;;        :deltaLink ((keyword "@odata.deltaLink") response)} 
+;;       (recur (:body (call-api ((keyword "@odata.nextLink") response)))
+;;              (concat results (:value response))))))
 
 
-(defn api-get-delta-callback
-  "Delta query enables applications to discover newly created, updated, or deleted
-  entities without performing a full read of the target resource with every request.
-  Microsoft Graph applications can use delta query to efficiently synchronize changes
-  with a local data store.
-  See docs at https://docs.microsoft.com/en-us/graph/delta-query-overview
-  Returns map {:resuls [] :deltaLink url} " 
-  [url fn]
-  (loop [response (:body (call-api url))
-         results  (fn (:value response))]
-    (if (nil? ((keyword "@odata.nextLink") response))
-      {:deltaLink ((keyword "@odata.deltaLink") response)}
-      (recur (:body (call-api ((keyword "@odata.nextLink") response)))
-             (fn (:value response))))))
+;; (defn api-get-delta-callback
+;;   "Delta query enables applications to discover newly created, updated, or deleted
+;;   entities without performing a full read of the target resource with every request.
+;;   Microsoft Graph applications can use delta query to efficiently synchronize changes
+;;   with a local data store.
+;;   See docs at https://docs.microsoft.com/en-us/graph/delta-query-overview
+;;   Returns map {:resuls [] :deltaLink url} " 
+;;   [url fn]
+;;   (loop [response (:body (call-api url))
+;;          results  (fn (:value response))]
+;;     (if (nil? ((keyword "@odata.nextLink") response))
+;;       {:deltaLink ((keyword "@odata.deltaLink") response)}
+;;       (recur (:body (call-api ((keyword "@odata.nextLink") response)))
+;;              (fn (:value response))))))
 
 
 (defn get-users
   "Lists users in the organization."
-  []
-  (api-get "/users?$top=999"))
+  [token]
+  (api-get "/users?$top=999") token)
 
-(defn get-groups []
-  (api-get "/groups?$top=999"))
+(defn get-groups [token]
+  (api-get "/groups?$top=999") token)
 
-(defn get-domains []
-  (api-get "/domains"))
+(defn get-domains [token]
+  (api-get "/domains") token)
 
 (defn filter-with-mail [users]
   (filter #(not-empty (:mail %)) users))
 
 ;; Folders
 
-(defn mailFolders [user]
-  (api-get (str "/users/" (:id user) "/mailFolders")))
+(defn mailFolders [user token]
+  (api-get (str "/users/" (:id user) "/mailFolders")) token)
 
-(defn childFolders [user folder]
-  (api-get (str "/users/" (:id user) "/mailFolders/" (:id folder) "/childFolders")))
+(defn childFolders [user folder token]
+  (api-get (str "/users/" (:id user) "/mailFolders/" (:id folder) "/childFolders")) token)
 
 
-(defn get-all-childFolders [user folders]
+(defn get-all-childFolders [user folders token]
   (loop [with-children (filter #(not=  0 (:childFolderCount %)) folders)
-         response (flatten (filter not-empty (map #(childFolders user %) with-children)))
+         response (flatten (filter not-empty (map #(childFolders user % token) with-children)))
          children []]
     (if (empty? with-children)
       (concat children response)
       (recur (filter #(not= 0 (:childFolderCount %)) response)
-             (flatten (filter not-empty (map #(childFolders user %) with-children)))
+             (flatten (filter not-empty (map #(childFolders user % token) with-children)))
              (concat children response) ))))
 
-(defn get-user-folders [user]
-  (let [folders (mailFolders user)
-        children (get-all-childFolders user folders)
+(defn get-user-folders [user token]
+  (let [folders (mailFolders user token)
+        children (get-all-childFolders user folders token)
         all (concat folders children)]
     all))
 
-(defn get-group-conversations [group]
-  (api-get (str "/groups/" (:id group) "/conversations")))
+;; (defn get-group-conversations [group]
+;;   (api-get (str "/groups/" (:id group) "/conversations")))
 
 ;; Messages
 
-(defn messages [user folder]
-  (api-get (str "/users/" (:id user) "/mailFolders/" (:id folder) "/messages?$top=999")))
+(defn messages [user folder token]
+  (api-get (str "/users/" (:id user) "/mailFolders/" (:id folder) "/messages?$top=999") token))
 
-(defn messages-since [user folder date]
+(defn messages-since [user folder date token]
   "date format ?$filter=ReceivedDateTime ge 2019-04-01"
-  (api-get (str "/users/" (:id user) "/mailFolders/" (:id folder) "/messages?$filter=ReceivedDateTime ge " date)))
+  (api-get
+   (str "/users/" (:id user) "/mailFolders/" (:id folder) "/messages?$filter=ReceivedDateTime ge " date)
+   token))
 
-(defn messages-with-state [user folder state]
+(defn messages-with-state [user folder state token]
   (if (:initial state)
-    (messages user folder)
-    (messages-since user folder (:time-started state))))
+    (messages user folder token)
+    (messages-since user folder (:time-started state) token)))
 
-(defn messages-callback [user folder fn]
-  (when-not (zero? (:totalItemCount folder))
-    (log/info "geting messages in folder " (:displayName folder) " totalItemCount:" (:totalItemCount folder) )
-    (api-get-callback (str "/users/" (:id user) "/mailFolders/" (:id folder) "/messages?$top=500") user fn)))
+;; (defn messages-callback [user folder fn]
+;;   (when-not (zero? (:totalItemCount folder))
+;;     (log/info "geting messages in folder " (:displayName folder) " totalItemCount:" (:totalItemCount folder) )
+;;     (api-get-callback (str "/users/" (:id user) "/mailFolders/" (:id folder) "/messages?$top=500") user fn)))
 
 
-(defn get-all-messages [user]
-  (doseq [folder (get-user-folders user)] (messages user folder)))
+(defn get-all-messages [user token]
+  (doseq [folder (get-user-folders user token)] (messages user folder token)))
 
-(defn accounts []
-  (let [users (get-users)
-        groups (get-groups)]
+(defn accounts [token]
+  (let [users (get-users token)
+        groups (get-groups token)]
     (concat users groups)))
 
 (defn totalItemCount [folders]
